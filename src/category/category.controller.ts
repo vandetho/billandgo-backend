966fd05b1a06f8e67287155a0029f551725f3053
Vandetho <thovandeth@gmail.com>
2024-02-04 13:26:27 +0700

    Implement update functionality for Category
    An update method is introduced in the Category service and controller to handle updating category details. This is supported by a new UpdateCategoryDto. Additionally, CategoryProducts retrieval methods are added into the Category Service, and Product schema is imported into Category Schema.

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CategoryService } from '@/billandgo/category/category.service';
 import { CreateCategoryDto } from '@/billandgo/category/dto/create-category.dto';
+import { UpdateCategoryDto } from '@/billandgo/category/dto/update-category.dto';
 
 @Controller('api/categories')
 export class CategoryController {
@@ -19,5 +20,10 @@ export class CategoryController {
     @Get(':categoryId')
     getCategory(@Param('categoryId') categoryId: string) {
         return this.categoryService.getCategory(categoryId);
+    }
+
+    @Put(':categoryId')
+    updateCategory(@Param('categoryId') categoryId: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+        return this.categoryService.update(categoryId, updateCategoryDto);
     }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -3,6 +3,7 @@ import { Category } from '@/billandgo/category/schema/category.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCategoryDto } from '@/billandgo/category/dto/create-category.dto';
+import { UpdateCategoryDto } from '@/billandgo/category/dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -19,5 +20,13 @@ export class CategoryService {
 
     getCategory(categoryId: string) {
         return this.categoryModel.findById(categoryId).exec();
+    }
+
+    getCategoryProducts(categoryId: string) {
+        return this.categoryModel.findById(categoryId).populate('products').exec();
+    }
+
+    update(categoryId: string, updateCategoryDto: UpdateCategoryDto) {
+        return this.categoryModel.findByIdAndUpdate(categoryId, updateCategoryDto, { new: true }).exec();
     }
 }
